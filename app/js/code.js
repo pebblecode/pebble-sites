f894766924305c6f6c346875a5f41cae7bb20401
Mark Durrant <mark@pebblecode.com>
2013-10-29 14:06:56 +0000

    bring .it in line

--- app/js/code.js
+++
@@ -1,5 +0,0 @@
-/*global $:true, console:true, Handlebars:true, JST:true*/
-
-buildTemplate( 'app/templates/style-guide.hbs', 'data/code-styles.json', '.wrap');
-
-buildTemplate( 'app/templates/header.hbs', 'data/code-header.json', '.site-header');
--- app/js/it.js
+++
@@ -1,3 +0,0 @@
-/*global $:true, console:true, Handlebars:true, JST:true*/
-
-buildTemplate( 'app/templates/header.hbs', 'data/it-header.json', '.site-header');
