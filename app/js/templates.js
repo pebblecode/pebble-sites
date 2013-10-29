1384914367d2f9c0486dceb187c0c3f0f51e976c
Mark Durrant <mark@pebblecode.com>
2013-10-29 18:13:36 +0000

    more 3d header & .it intergration

--- app/js/templates.js
+++ app/js/templates.js
@@ -12,15 +12,11 @@ function program1(depth0,data) {
   if (stack1 = helpers.page) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.page; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + ".html\" class=\"nav-item\">";
+    + ".html\" class=\"nav-item\" data-page=\"";
   if (stack1 = helpers.page) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.page; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\n          <span data-page=\"";
-  if (stack1 = helpers.page) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.page; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "\">";
+    + "\">\n          <span>";
   if (stack1 = helpers.page) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.page; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
