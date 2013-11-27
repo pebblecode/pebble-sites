d0fb184dace6863cde0cee6d4a851dbdf9799fe8
Mark Durrant <mark@Daniels-Mac.local>
2013-11-27 17:00:38 +0000

    more gh-pages stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,7 @@ module.exports = function ( grunt ) {
 
     'gh-pages': {
       options: {
-        base: [ 'dist/*.html', 'dist/code/**', 'dist/it/**' ]
+        base: 'dist/'
       },
 
       src: [ '*.html', 'js/**/*', 'css/**/*', 'img/**/*' ]
